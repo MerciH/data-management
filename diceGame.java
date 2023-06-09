5ead437e98fb0e968f5bb6831215d1d68b5d4943
Merci <habmercima@gmail.com>
2023-06-09 11:57:51 +0900

    Won/lost message modified

--- diceGame.java
+++ diceGame.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class diceGame{
     private int max = 6;
     private int min = 1;
+    private String name;
     public int rollDice(){
         Random random = new Random();
         return random.nextInt(max-min)+min;
     }
     public static void main(String[] args){
-        
-        try (Scanner sc = new Scanner(System.in)) {
+        diceGame dg = new diceGame();
+        Scanner sc = new Scanner(System.in);
             try{
                 System.out.println("What is your name?");
-                String name = sc.nextLine();
-                System.out.println("Hello, "+name);
+                dg.name = sc.nextLine();
+                System.out.println("Hello, "+dg.name);
             }
             catch(Exception e){
                 System.out.println("Wrong input!");
             }
-        }
-        diceGame dg = new diceGame();
+        
         int dieOne = dg.rollDice();
         int dieTwo = dg.rollDice();
         int sum = dieOne+dieTwo;
@@ -30,8 +30,8 @@ public class diceGame{
         System.out.println("Die 2 : "+dieTwo);
         System.out.println("Total Value: "+sum);
         if (sum>7){
-            System.out.println("You Won!");
+            System.out.println(dg.name + " Won!");
         }
-        else System.out.println("You Lost!");
+        else System.out.println(dg.name+" Lost!");
     }
 }
